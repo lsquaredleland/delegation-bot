6e6946e33ce5ef61a2db40331b89379fdcb4adfd
leland <lelandlee4@gmail.com>
2020-11-03 13:34:45 -0800

    removed @mention to get unblocked by Twitter

--- src/data/mapping.js
+++ src/data/mapping.js
@@ -1,50 +1,50 @@
 export const UniDelegates = {
-  '0x7e4a8391c728fed9069b2962699ab416628b19fa': '@Dharma_HQ',
-  '0x6626593c237f530d15ae9980a95ef938ac15c35c': '@gauntletnetwork',
+  '0x7e4a8391c728fed9069b2962699ab416628b19fa': 'Dharma_HQ', //@ - temporarily removed
+  '0x6626593c237f530d15ae9980a95ef938ac15c35c': 'gauntletnetwork', //@ - temporarily removed
   '0x1994b59e02cd0d09d839b03660c205e88a929b81': 'Andre Multisig',
-  '0x8d07d225a769b7af3a923481e1fdf49180e6a265': '@MonetSupply',
-  '0x2d407ddb06311396fe14d4b49da5f0471447d45c': '@AndreCronjeTech',
-  '0x88FB3D509fC49B515BFEb04e23f53ba339563981': '@rleshner', // might be switched later
-  '0x84bb5196F0085D61a7C9AC5B903a26D7f009aC13': '@chrisblec',
-  '0x0be0ecc301a1c0175f07a66243cff628c24db852': '@HiturunkE',
-  '0xC9f7d5870Cc691E4F8d25399EC10C90d55805AC7': '@bneiluj', // currently no delegates at all
+  '0x8d07d225a769b7af3a923481e1fdf49180e6a265': 'MonetSupply', //@ - temporarily removed
+  '0x2d407ddb06311396fe14d4b49da5f0471447d45c': 'AndreCronjeTech', //@ - temporarily removed
+  '0x88FB3D509fC49B515BFEb04e23f53ba339563981': 'rleshner', // might be switched later //@ - temporarily removed
+  '0x84bb5196F0085D61a7C9AC5B903a26D7f009aC13': 'chrisblec', //@ - temporarily removed
+  '0x0be0ecc301a1c0175f07a66243cff628c24db852': 'HiturunkE', //@ - temporarily removed
+  '0xC9f7d5870Cc691E4F8d25399EC10C90d55805AC7': 'bneiluj', // currently no delegates at all //@ - temporarily removed
 }
 
 export const CompDelegates = {
-  '0x9aa835bc7b8ce13b9b0c9764a52fbf71ac62ccf1':'@a16z',
-  '0xed409c9ff60f3020abf9012bcd45fc294f5608ff':'@polychaincap',
-  '0x6626593c237f530d15ae9980a95ef938ac15c35c':'@gauntletnetwork',
-  '0xa1b61405791170833070c0ea61ed28728a840241':'@paradigm',
-  '0xac5720d6ee2d7872b88914c9c5fa9bf38e72faf6':'@rleshner',
-  '0x8169522c2c57883e8ef80c498aab7820da539806':'@justHGH',
+  '0x9aa835bc7b8ce13b9b0c9764a52fbf71ac62ccf1':'a16z', //@ - temporarily removed
+  '0xed409c9ff60f3020abf9012bcd45fc294f5608ff':'polychaincap', //@ - temporarily removed
+  '0x6626593c237f530d15ae9980a95ef938ac15c35c':'gauntletnetwork', //@ - temporarily removed
+  '0xa1b61405791170833070c0ea61ed28728a840241':'paradigm', //@ - temporarily removed
+  '0xac5720d6ee2d7872b88914c9c5fa9bf38e72faf6':'rleshner', //@ - temporarily removed
+  '0x8169522c2c57883e8ef80c498aab7820da539806':'justHGH', //@ - temporarily removed
   '0x54a37d93e57c5da659f508069cf65a381b61e189':'blck',
-  '0x7e4a8391c728fed9069b2962699ab416628b19fa':'@Dharma_HQ',
+  '0x7e4a8391c728fed9069b2962699ab416628b19fa':'Dharma_HQ', //@ - temporarily removed
   '0x0cde9622b23ababfccef9755b5f7c9e759ef4820':'Set Pause Guardian to Community Multi-Sig',
-  '0x1d29756e8f7b091ce6c11a35980de79c7eda5d1f':'@InstaDApp',
-  '0x1d29756e8f7b091ce6c11a35980de79c7eda5d1f':'@paraficapital',
-  '0x5d8908afee1df9f7f0830105f8be828f97ce9e68':'@argentHQ',
-  '0x50495480cce225c8476e90466d8615a9e6a5e004':'@dragonfly_cap',
-  '0xd061ba96195923e49e619bc1c758568fe47a1fb1':'@IDEOVC',
-  '0x2fd6181541bebe30d17cf3a5d9f40ebcecbdba43':'@KyberNetwork',
-  '0xbe1f4dc51e89cf3f72cc4039bb0104eb96f85733':'@1inchExchange',
-  '0x879d430d8eba1de4cf1bad4e20da5e559c030f11':'@jaysonhobby',
+  '0x1d29756e8f7b091ce6c11a35980de79c7eda5d1f':'InstaDApp', //@ - temporarily removed
+  '0x1d29756e8f7b091ce6c11a35980de79c7eda5d1f':'paraficapital', //@ - temporarily removed
+  '0x5d8908afee1df9f7f0830105f8be828f97ce9e68':'argentHQ', //@ - temporarily removed
+  '0x50495480cce225c8476e90466d8615a9e6a5e004':'dragonfly_cap', //@ - temporarily removed
+  '0xd061ba96195923e49e619bc1c758568fe47a1fb1':'IDEOVC', //@ - temporarily removed
+  '0x2fd6181541bebe30d17cf3a5d9f40ebcecbdba43':'KyberNetwork', //@ - temporarily removed
+  '0xbe1f4dc51e89cf3f72cc4039bb0104eb96f85733':'1inchExchange', //@ - temporarily removed
+  '0x879d430d8eba1de4cf1bad4e20da5e559c030f11':'jaysonhobby', //@ - temporarily removed
   '0xe3c9ece23316b6b06142fa0ca915f02c323c6b21':'Dakeshi',
-  '0xe5b5cf0e5d044c3e984b910dbc89b19186d24b03':'@PanteraCapital',
-  '0x0a09cd09b0107bb98a83f211704f036eca94b92e':'@PoolTogether_',
-  '0x5b97680e165b4dbf5c45f4ff4241e85f418c66c2':'@kaiynne',
-  '0xfdaae0c5a7a56c8265b9f86cf8cd934a494e3733':'@RyanSAdams',
-  '0xfdaae0c5a7a56c8265b9f86cf8cd934a494e3733':'@maxcwolff',
-  '0xbbd13ca6aace2a8eccbde88bc7849c3c6e4e172e':'@burton_ric',
-  '0xcfae402f6ced5a476554235b5026c005c85c135a':'@DeFiRate',
-  '0xb0325dbe7fa891436e83a094f9f12848c78e449b':'@cjliu49',
-  '0x7de05261d0b952a19de6a9ffc4f77bab447aa044':'@lukeyoungblood',
-  '0x2b384212edc04ae8bb41738d05ba20e33277bf33':'@Arr00c',
+  '0xe5b5cf0e5d044c3e984b910dbc89b19186d24b03':'PanteraCapital', //@ - temporarily removed
+  '0x0a09cd09b0107bb98a83f211704f036eca94b92e':'PoolTogether_', //@ - temporarily removed
+  '0x5b97680e165b4dbf5c45f4ff4241e85f418c66c2':'kaiynne', //@ - temporarily removed
+  '0xfdaae0c5a7a56c8265b9f86cf8cd934a494e3733':'RyanSAdams', //@ - temporarily removed
+  '0xfdaae0c5a7a56c8265b9f86cf8cd934a494e3733':'maxcwolff', //@ - temporarily removed
+  '0xbbd13ca6aace2a8eccbde88bc7849c3c6e4e172e':'burton_ric', //@ - temporarily removed
+  '0xcfae402f6ced5a476554235b5026c005c85c135a':'DeFiRate', //@ - temporarily removed
+  '0xb0325dbe7fa891436e83a094f9f12848c78e449b':'cjliu49', //@ - temporarily removed
+  '0x7de05261d0b952a19de6a9ffc4f77bab447aa044':'lukeyoungblood', //@ - temporarily removed
+  '0x2b384212edc04ae8bb41738d05ba20e33277bf33':'Arr00c', //@ - temporarily removed
   '0xc3aae58ab81663872dd36d73613eb295b167f546':'TennisBowling',
-  '0xa8c6f6e2dfbdededd157a5363d38d7d074d83985':'@ConsenSys',
-  '0x18c8f1222083997405f2e482338a4650ac02e1d6':'@jmflatow',
-  '0x8d07d225a769b7af3a923481e1fdf49180e6a265':'@MonetSupply',
-  '0xb8a61ce672971ed30af21c923e2d86ac3e2e6406':'@veradittakit',
-  '0x55e936c55ac259bf6269f461c0ed1fb293baa157':'@RAC',
-  '0xfc04833ca66b7d6b4f540d4c2544228f64a25ac2':'@nickmartitsch',
+  '0xa8c6f6e2dfbdededd157a5363d38d7d074d83985':'ConsenSys', //@ - temporarily removed
+  '0x18c8f1222083997405f2e482338a4650ac02e1d6':'jmflatow', //@ - temporarily removed
+  '0x8d07d225a769b7af3a923481e1fdf49180e6a265':'MonetSupply', //@ - temporarily removed
+  '0xb8a61ce672971ed30af21c923e2d86ac3e2e6406':'veradittakit', //@ - temporarily removed
+  '0x55e936c55ac259bf6269f461c0ed1fb293baa157':'RAC', //@ - temporarily removed
+  '0xfc04833ca66b7d6b4f540d4c2544228f64a25ac2':'nickmartitsch', //@ - temporarily removed
   '0x87887cf0e37d937f989ab76b99e4f4682da044c4':'opyn_',
 }
